71e1371c47971a6c63b950af66cedd088ed8daa5
ANSHU2692 <anshugour1999@gmail.com>
2025-07-26 11:40:35 +0530

    update

--- 2Darray.cpp
+++ 2Darray.cpp
@@ -32,60 +32,308 @@ using namespace std;
 
 //spiral matrix problem
 
-void spiralMatrix(int mat[][4],int n,int m){
+// void spiralMatrix(int mat[][4],int n,int m){
 
-    int srow = 0 , erow = n-1;
-    int sclm = 0 , eclm = m-1;
+//     int srow = 0 , erow = n-1;
+//     int sclm = 0 , eclm = m-1;
     
-    while (srow<=erow && sclm<=eclm){
-          //top
-
-            for(int i=sclm;i<=eclm;i++){
-                cout<<mat[srow][i]<<" ";
-            }
-            //right
-            for(int j=srow+1;j<=erow;j++){
-                cout<<mat[j][eclm]<<" ";
-            }
-
-            //bottom
-
-            for(int i=eclm-1;i>=sclm;i--){
-                if(srow==erow){
-                    break;
-                }
-                cout<<mat[erow][i]<<" ";
-            }
-            //left
-
-            for(int j=erow-1;j>=srow+1;j--){
-                  if(sclm==eclm){
-                    break;
-                }
-                cout<<mat[j][sclm]<<" ";
-            }
-
-
-            srow++; sclm++;
-            erow--; eclm--;
-    }
-    
-
-}
+//     while (srow<=erow && sclm<=eclm){
+//             //top
+//             for(int i=sclm;i<=eclm;i++){
+//                 cout<<mat[srow][i]<<" ";
+//             }
+//             //right
+//             for(int j=srow+1;j<=erow;j++){
+//                 cout<<mat[j][eclm]<<" ";
+//             }
+//             //bottom
+//             for(int i=eclm-1;i>=sclm;i--){
+//                 if(srow==erow){
+//                     break;
+//                 }
+//                 cout<<mat[erow][i]<<" ";
+//             }
+//             //left
+//             for(int j=erow-1;j>=srow+1;j--){
+//                   if(sclm==eclm){
+//                     break;
+//                 }
+//                 cout<<mat[j][sclm]<<" ";
+//             }
+//             srow++; sclm++;
+//             erow--; eclm--;
+//     }
+// }
 
   
-int main(){
+// int main(){
+//     int matrix[5][4]={  {1,2,3,4},
+//                         {5,6,7,8,},
+//                         {9,10,11,12},
+//                         {13,14,15,16},
+//                         {14,15,16,17}   };
 
-    int matrix[5][4]={  {1,2,3,4},
-                        {5,6,7,8,},
-                        {9,10,11,12},
-                        {13,14,15,16},
-                        {14,15,16,17}   };
-
-    spiralMatrix(matrix,5,4);
+//     spiralMatrix(matrix,5,4);
+//     return 0;
+// }
 
 
+// diagonal sum  o[n^2]
 
-    
-    return 0;
-}
+// void digSum(int mat[][3],int n){
+//     int sum = 0;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             if (i==j)
+//             {
+//                 sum += mat[i][j];
+//             }else if(j==n-i-1){
+//                 sum += mat[i][j];
+//             }
+//         }
+//     }
+//     cout<<sum;
+// }
+
+
+// bit optimized  o[n]
+
+// void digsum(int mat[][3],int n){
+//     int sum =0;
+//     for(int i=0;i<n;i++){
+//         sum += mat[i][i];
+//         if(i != n-i-1){
+//             sum += mat[i][n-i-1];
+//         }
+//     }
+//     cout<< sum;
+
+// }
+
+// int main(){
+//     int matrix[3][3]={  {1,2,3},
+//                         {4,5,6},
+//                         {7,8,9}   };
+
+//     digSum(matrix,3);
+
+//     digsum(matrix,3);
+
+
+//     return 0;
+// }
+
+
+
+//  searching in sorted matrix
+
+// int brutSearch(int matrx[][4],int n ,int m , int key){
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             if(matrx[i][j]==key){
+//                 cout<<i<<","<<j;
+//             }
+//         }
+//     }
+
+// }
+
+// int main(){
+//     int matrix[4][4]={{10,20,30,40},{15,25,35,45},{27,29,37,38},{32,33,39,50}};
+
+//     brutSearch(matrix,4,4,33);
+//     return 0;
+// }
+
+// // Function to perform binary search on a single row
+// bool binarySearchRow(int row[], int cols, int target) {
+//     int low = 0, high = cols - 1;
+
+//     // Binary Search Logic
+//     while (low <= high) {
+//         int mid = (low + high) / 2;
+
+//         // If element found
+//         if (row[mid] == target)
+//             return true;
+//         // If target is greater, ignore left half
+//         else if (row[mid] < target)
+//             low = mid + 1;
+//         // If target is smaller, ignore right half
+//         else
+//             high = mid - 1;
+//     }
+//     // Element not found in this row
+//     return false;
+// }
+
+
+
+
+
+
+// binarySearch in matrix
+// int binarySearchRow(int row[], int cols, int target) {
+//     int left = 0, right = cols - 1;
+
+//     while (left <= right) {
+//         int mid = (left + right) / 2;
+
+//         if (row[mid] == target)
+//             return mid; 
+//         else if (row[mid] < target)
+//             left = mid + 1; 
+//         else
+//             right = mid - 1;
+//     }
+
+//     return -1; 
+// }
+
+
+// bool rowWiseBinarySearch(int matrix[][4], int rows, int cols, int target) {
+//     for (int i = 0; i < rows; i++) {
+//         int colIndex = binarySearchRow(matrix[i], cols, target);
+//         if (colIndex != -1) {
+//             cout << "Target " << target << " found at index: (" << i << ", " << colIndex << ")" << endl;
+//             return true;
+//         }
+//     }
+
+//     cout << "Target " << target << " not found in the matrix." << endl;
+//     return false;
+// }
+
+// int main() {
+
+//     int matrix[4][4] = {
+//         {10, 20, 30, 40},
+//         {15, 25, 35, 45},
+//         {27, 29, 37, 38},
+//         {32, 33, 39, 50}
+//     };
+
+//     int target = 33; 
+
+//     rowWiseBinarySearch(matrix, 4, 4, target);
+
+//     return 0;
+// }
+
+
+// stare case problem
+// for starting cell top-right
+
+// bool staircaseSearch(int mat[][4],int n ,int m ,int key){
+//     int row=0,colm=m-1;
+   
+//     while(row<n && colm>=0){
+//         int cell = mat[row][colm];
+//         if (cell==key){
+//             cout<<"cell value"<<row<<","<<colm<<"\n";
+//             return true;
+//         }else if(cell>key){
+//             colm--;
+//         }else if (cell<key){
+//             row++;
+//         }
+//     }
+//     return false;
+// }
+
+// int main(){
+//     int matrix [4][4] ={{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+
+//     cout<< staircaseSearch(matrix,4,4,50);
+
+//     return 0;
+// }
+
+// for bottom left 
+
+// bool staircaseSearch(int mat[][4],int n ,int m ,int key){
+//     int row=n-1,colm=0;
+   
+//     while(row>=0 && colm<m){
+//         int cell = mat[row][colm];
+//         if (cell==key){
+//             cout<<"cell value"<<row<<","<<colm<<"\n";
+//             return true;
+//         }else if(cell>key){
+//              row--;
+//         }else if (cell<key){
+//             colm++;   
+//         }
+//     }
+//     return false;
+// }
+
+// int main(){
+//     int matrix [4][4] ={{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+
+//     cout<< staircaseSearch(matrix,4,4,33);
+
+//     return 0;
+// }
+
+
+// assignment
+// int count1(int mat[][3],int n,int m ,int key){
+//     int count =0;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             if(mat[i][j]==key){
+//                 count ++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// int sumRow(int mat[][3],int n , int m){
+//     int sum = 0;n=1;
+
+//     for(int i=0;i<m;i++){
+//         sum += mat[n][i];
+//     }
+//     return sum;
+// }
+
+// transpose of matrix
+
+// int transpose(int mat[][3],int n, int m){
+//     int transpose[m][n];
+
+//     for(int i=0;i<n;i++){
+//         for (int j=0;j<m;j++){
+//             transpose[j][i]=mat[i][j];
+//         }
+//     }
+//     for(int i=0;i<m;i++){
+//         for (int j=0;j<n;j++){
+//            cout<< transpose[i][j]<<" ";
+//         }
+//         cout<<"\n";
+//     }
+
+// }
+
+// int main(){
+//     // int arr[2][3] ={{4,7,8},{8,8,7}};
+//     // cout<<count1(arr,4,4,7);
+
+//     // int nums[3][3]={{1,4,9},{11,4,3},{2,2,3}};
+//     // cout<<sumRow(nums,3,3);
+
+//     int arr[2][3]={{2,3,7},{5,6,7}};
+//     transpose(arr,2,3);
+
+
+// }
+
+
+
+
+
+
+
---
+++ charArray.cpp
@@ -0,0 +1,277 @@
+#include<iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+// int main(){
+//     //  index of alphabets
+//     // char ch='c';
+//     // int position1=ch-'a';
+//     // cout<< position1<<"\n"; 
+//     // char ch1='G';
+//     // int position2=ch1-'A';
+//     // cout<< position2; 
+//     // char str[5]={'a','b','c','d','e'};
+//     // cout<<str<<endl; // in int arr we get address but in char arr we get whole value 
+//     // char work[]="code";
+//     // cout<<work<<endl;
+//     // cout<< strlen(work)<<endl;
+//     // char str1[5]="code";
+//     // cout<<str<<endl;
+//     // char str2[]={'c','o','d','e','\0'};
+//     // cout<<str2<<endl;
+//     // char str3[50]={'c','o','d','e','\0'};
+//     // cout<<str3<<endl;
+//     // char work[30];
+//     // cin>>work;      //ignore whitespaces
+//     // cout<<"the word enter is :"<<work<<endl;
+//     // cout<<strlen(work);
+//     // char work[30];
+//     // cin.getline(work,30,'.');     
+//     // cout<<"the word enter is :"<<work<<endl;
+//     // cout<<strlen(work);
+//     return 0;
+// }
+// to upper case
+// void Toupprecse(char str[],int n){
+//     for(int i=0;i<n;i++){
+//         char ch =str[i];
+//         if(ch>='A' && ch<='Z'){
+//             continue;
+//         }else{
+//             str[i]=ch-'a'+'A';
+//         }
+//     }
+
+// }
+// int main(){
+//     char word[]="ApPle";
+//     Toupprecse(word,strlen(word));
+//     cout<<word;
+
+// }
+//to lower case
+// void Tolowercase(char str[],int n){
+//     for(int i=0;i<n;i++){
+//         char ch =str[i];
+//         if(ch>='a' && ch<='z'){
+//             continue;
+//         }else{
+//             str[i]=ch-'A'+'a';
+//         }
+//     }
+
+// }
+// int main(){
+//     char word[]="ApPle";
+//     Tolowercase(word,strlen(word));
+//     cout<<word;
+
+// }
+
+// void revcharr(char str[],int n){
+//     int start=0;
+//     int end=n-1;
+
+//     // while (start<end)
+//     // {
+//     //     char temp=str[start];
+//     //     str[start]=str[end];
+//     //     str[end]=temp;
+//     //     start++;
+//     //     end--;
+//     // }
+//     while (start<end)
+//     {
+//         swap(str[start],str[end]);
+//         start++;
+//         end--;
+//     }
+// }
+// int main(){
+//     char word[5]="code";
+//     revcharr(word,strlen(word));
+//     cout<<word;
+// }
+
+
+// bool ispalendrome(char str[],int n){
+//     int start=0;
+//     int end=n-1;
+
+//     while(start<end){
+//         if(str[start] != str[end]){
+//              cout<<"it is a not palandrome";
+//              return false;
+//         }start++;
+//         end--;   
+//     }
+
+//     cout<<"it is palandrome";
+//     return true;
+// }
+// int main(){
+//     char word[]="abbabbba";
+
+//     ispalendrome(word,strlen(word));
+
+// }
+
+
+//int main(){
+    //     char str[50];
+    //     strcpy(str,"apple");
+    //     char str1[]="anshu;
+    //     char str2[]="gour";
+    //     strcat(str1,str2);
+    //    cout<< strcmp(str1,str2)<<endl;
+    // cout<< str1;
+// string str = "my name is anshu gour!";
+// getline(cin,str);
+// cout<<str;
+// cout<<str.length();
+// for(int i=0;i<str.length();i++){
+//     cout<<str[i]<<"-";
+// }
+// for(char ch: str){
+//      cout<< ch << "-";   
+//     }
+// cout<< str.at(5)<<"\n";
+// cout<< str.substr(2,5)<<"\n";
+// cout<<str.find("anshu");
+//}
+
+// valid anagarm.
+// bool isvalid(string str1,string str2){
+//     if(str1.length() != str2.length()){
+//         cout<<"not a valid anagarm!";
+//         return false;
+//     }
+//     int count[26]={0};
+
+//     for(int i=0;i<str1.length();i++){ 
+//        count[str1[i]-'a']++;
+//     }
+
+//     for(int i=0;i<str2.length();i++){
+//         if(count[str2[i]-'a']==0){
+//             cout<<"not a valid anagram!";
+//             return false;
+//         }
+//         count[str2[i]-'a']--;
+//     }
+//     cout<<"valid anagram";
+//     return true;
+// }
+// int main(){
+//     string str1="car";
+//     string str2="rac";
+
+//     isvalid(str1,str2);
+
+//     return 0;
+// }
+
+
+// void checkvovel(string str){
+//     int count=0;
+//     for(int i=0;i<str.length();i++){
+//         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+//             count++;
+//         }
+//     }
+//     cout<< count;
+// }
+
+// int main(){
+//     string str = "my name is anshu gour";
+
+//     checkvovel(str);
+
+//     return 0;
+// }
+
+// bool operswap(string str1,string str2){
+//     int n=str2.length();
+   
+//     // Try all pairs (i, j) for a possible swap
+//     for (int i = 0; i < n; ++i) {
+//         for (int j = i; j < n; ++j) {
+//             swap(str2[i], str2[j]); // Swap
+//             if (str2 == str1) return true; // Check match
+//             swap(str2[i], str2[j]); // Undo swap
+//         }
+    
+
+//     return false;
+//     }
+// }
+// int main(){
+//     string str1="bank";
+//     string str2="kanb";
+//     if (operswap(str1, str2))
+//         cout << "true\n";
+//     else
+//         cout << "false\n";
+
+//     return 0;
+// }
+
+
+
+// bool areAlmostEqual(string s1, string s2) {
+//     if (s1.length() != s2.length()) {
+//         return false; // Strings of different lengths can't be made equal by a swap
+//     }
+
+//     char diffChar1, diffChar2;
+//     int diff = 0;
+
+//     for (int i = 0; i < s1.length(); i++) {
+//         if (s1[i] != s2[i]) {
+//             if (diff == 0) {
+//                 // First mismatch — store the mismatched characters
+//                 diffChar1 = s1[i];
+//                 diffChar2 = s2[i];
+//             } else {
+//                 // Second mismatch — check if swapping would fix the mismatch
+//                 if (s1[i] != diffChar2 || s2[i] != diffChar1) {
+//                     return false;
+//                 }
+//             }
+//             diff++;
+
+//             // More than 2 mismatches means we can't fix with one swap
+//             if (diff > 2) {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     // Only two mismatches can be fixed with one swap; one mismatch is not enough
+//     if (diff == 1) {
+//         return false;
+//     }
+
+//     return true;
+// }
+
+// int main(){
+//     string s1="bank";
+//     string s2="kanb";
+
+//      if (areAlmostEqual(s1, s2)) {
+//         cout << "True\n"; // Expected output: True
+//     } else {
+//         cout << "False\n";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
